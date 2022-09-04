d3ab22117295a971e4e801d4a0b66fe0d1b3994e
Thalia-Dev07 <thaliaoficial7@gmail.com>
2022-09-04 19:54:14 -0300

    segundo-comite

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGAYeFhnf5nlZjk7NTM-lgjVVP6PfPEyU",
+  authDomain: "projeto-teste-d2f0b.firebaseapp.com",
+  projectId: "projeto-teste-d2f0b",
+  storageBucket: "projeto-teste-d2f0b.appspot.com",
+  messagingSenderId: "552262491470",
+  appId: "1:552262491470:web:f7d49757285f064b18921f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+
+import { getHellfireClubSubscriptions, subscribeToHellfireClub } from './firebase/hellfire-clube.js'
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', async () => {
+    const subscription = {
+        name: txtName.value, 
+        email:txtEmail.value, 
+        level:txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    // salvar no banco de dados
+  const subscriptionId = await subscribeToHellfireClub(subscription) 
+
+    txtName.value = ''
+    txtEmail.value = ''
+    txtLevel.value = ''
+    txtCharacter.value = ''
+
+    alert(`Inscrito com sucesso: ${subscriptionId}`)
+})
+
+async function loadData() {
+    const subscriptions = await getHellfireClubSubscriptions()
+    console.log(subscriptions)
+}
+
+loadData()
